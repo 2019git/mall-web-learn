5e89adab9ebd07bbf56da7230af1dfce876a44da
wangzhijun <wangzhijun@DESKTOP-H0VSEF1>
2021-04-22 19:25:33 +0800

    导航栏添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,25 @@
 <template>
-    <H2>首页</H2>
+  <div>
+    <nav-bar class="nav-bar-home">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from '@/components/common/navbar/NavBar.vue'
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
     }
+  }
 </script>
 
 <style scoped>
-
+  .nav-bar-home {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
