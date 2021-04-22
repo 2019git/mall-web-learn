8f9b255b1483e57ed967a1554f24340f3a32df1b
wangzhijun <wangzhijun@DESKTOP-H0VSEF1>
2021-04-22 16:35:23 +0800

    tabbar添加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
+    <TabBarMain></TabBarMain>
   </div>
 </template>
+
+<script>
+  import TabBarMain from '@/components/content/TabBarMain.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      TabBarMain
+    }
+  }
+</script>
 
 <style>
   @import  "./assets/css/base.css";
