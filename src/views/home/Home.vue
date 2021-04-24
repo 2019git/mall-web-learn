db2c665b53a214a5e3cfa14951d8c351018c3997
wzj <123>
2021-04-24 23:54:15 +0800

    首页轮播图和推荐信息展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,36 @@
     <nav-bar class="nav-bar-home">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
   </div>
 </template>
 
 <script>
   import NavBar from '@/components/common/navbar/NavBar.vue'
+  import HomeSwiper from './childrenviews/HomeSwiper'
+  import RecommendView from './childrenviews/RecommendView'
+
+  import {getHomeMultidata} from '@/network/home.js'
 
   export default {
     name: "Home",
     components: {
+      HomeSwiper,
+      RecommendView,
       NavBar
+    },
+    data() {
+      return {
+        banner: [],
+        recommend: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      })
     }
   }
 </script>
---
+++ src/views/home/childrenviews/RecommendView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommend" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommendView",
+    props: {
+      recommend: {
+        type: Array,
+        default: function () {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    /*padding 简写属性在一个声明中设置所有内边距属性*/
+    padding: 10px 0 20px;
+    /*设置下边框的样式*/
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 70px;
+    height: 70px;
+    /*设置元素的下外边距*/
+    margin-bottom: 10px;
+  }
+</style>
