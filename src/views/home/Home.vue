bd2898508d3d2f6d6d273532128e0f6dfe9c48fc
wangzhijun <wangzhijun@DESKTOP-H0VSEF1>
2021-04-25 18:54:36 +0800

    tab-control实现

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,59 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="nav-bar-home">
       <div slot="center">购物街</div>
     </nav-bar>
     <home-swiper :banner="banner"></home-swiper>
     <recommend-view :recommend="recommend"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from '@/components/common/navbar/NavBar.vue'
+  import TabControl from '@/components/content/tabControl/TabControl'
   import HomeSwiper from './childrenviews/HomeSwiper'
   import RecommendView from './childrenviews/RecommendView'
+  import FeatureView from './childrenviews/FeatureView'
 
   import {getHomeMultidata} from '@/network/home.js'
 
@@ -20,7 +62,10 @@
     components: {
       HomeSwiper,
       RecommendView,
-      NavBar
+      FeatureView,
+
+      NavBar,
+      TabControl
     },
     data() {
       return {
@@ -38,8 +83,24 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .nav-bar-home {
     background-color: var(--color-tint);
     color: white;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 0;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 40px;
+    z-index: 9;
   }
 </style>
