f67940a80fa444a8d05dbc125ea8617d62dd7bd6
wzj <123>
2021-05-31 23:36:37 +0800

    vuex添加

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Mutations from './Mutations'
+import Getters from './Getters'
+import Actions from './Actions'
+
 
 Vue.use(Vuex)
 
+const state = {}
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: state,
+  mutations: Mutations,
+  getters: Getters,
+  actions: Actions,
 })
